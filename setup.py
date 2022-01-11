ccdd433c3a9281a1f3041eec04d1f0783f5d9207
Brian Waligorski <bwaligorski@inquirer.com>
2022-01-11 14:22:20 -0500

    remove comment block

--- setup.py
+++ setup.py
@@ -2,27 +2,3 @@ import setuptools
 
 if __name__ == "__main__":
     setuptools.setup()
-
-'''
-from setuptools import setup, find_packages
-
-
-setup(
-    name='vestapol',
-    version='0.1.0',
-    description=(
-        'Python package that loads data from the web and deploys a'
-        ' corresponding external table definition, so that the data can be'
-        ' queried using standard SQL.'
-    ),
-    # TODO: edit so that install doesn't require gcp client libs
-    install_requires=[
-        'requests>=2.26.0',
-        'google-cloud-storage>=1.43.0',
-        'google-cloud-bigquery>=2.31.0'
-    ],
-    url='http://github.com/phillymedia/vestapol',
-    packages=find_packages(),
-    zip_safe=False
-)
-'''
