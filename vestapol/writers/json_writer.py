64484228ed96b8f13449eb359e8bd11ad73d5846
Brian Waligorski <bwaligorski@inquirer.com>
2022-01-12 15:43:31 -0500

    move type handling to resources

--- vestapol/writers/json_writer.py
+++ vestapol/writers/json_writer.py
@@ -6,17 +6,10 @@ logger = logging.getLogger(__name__)
 
 
 def write_json(data: dict, pathname, destination):
-    if not isinstance(data, dict):
-        raise TypeError(f"Expected a dict, received {type(data)}")
-
     data_string = json.dumps(data)
     destination.write_data(data_string, pathname)
 
 
 def write_jsonl(data: List[Dict], pathname, destination):
-    if not isinstance(data, list):
-        raise TypeError(f"Expected a list, received {type(data)}")
-    
     data_string = "\n".join([json.dumps(record) for record in data])
-
     destination.write_data(data_string, pathname)
