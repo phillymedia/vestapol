89b70a772879bd820684d2d3a4b38d05beef4e2d
EvavW <eva.vonweltin@gmail.com>
2022-05-25 11:19:48 -0400

    move destination and resource aggregation

--- vestapol/typing/destinations.py
+++
@@ -1,5 +0,0 @@
-from vestapol.destinations.gcp_destination import GoogleCloudPlatform
-from vestapol.destinations.local_destination import Local
-from typing import Union
-
-DestinationTypes = Union[GoogleCloudPlatform, Local]
--- vestapol/typing/resources.py
+++
@@ -1,6 +0,0 @@
-from vestapol.web_resources.csv_resource import CSVResource
-from vestapol.web_resources.github_resource import GitHubResource
-from vestapol.web_resources.json_resource import JSONResource
-from typing import Union
-
-ResourceTypes = Union[CSVResource, GitHubResource, JSONResource]
