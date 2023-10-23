cc6f0c54f04b5ef909ad3571900cfd9a7cb34672
ajonesINQ <ajones@inquirer.com>
2023-10-23 11:50:29 -0400

    :white_checkmark: Correcting typos and adding tests

--- tests/web_resources/test_csv_resource.py
+++ tests/web_resources/test_csv_resource.py
@@ -16,8 +16,9 @@ def mock_csv_resource():
     version = "v99.9"
     skip_leading_rows = 123
     allow_quoted_newlines = True
+    specified_delimiter = ","
     mock_csv_resource = csv_resource.CSVResource(
-        name, base_url, endpoint, version, skip_leading_rows, allow_quoted_newlines
+        name, base_url, endpoint, version, skip_leading_rows, allow_quoted_newlines,specified_delimiter,
     )
     mock_csv_resource.requested_at = DateTime(1970, 1, 1)
     return mock_csv_resource
