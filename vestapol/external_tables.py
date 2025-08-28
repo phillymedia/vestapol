d09922d004fd5b85d1bab907a045e0d4569f961a
Valerie <vbelova@inquirer.com>
2025-08-28 12:20:24 -0400

    fixing formatting

--- vestapol/external_tables.py
+++ vestapol/external_tables.py
@@ -47,7 +47,7 @@ def get_external_data_configuration(
         external_config.csv_options.skip_leading_rows = skip_leading_rows  # type: ignore # noqa: E501
         external_config.csv_options.allow_quoted_newlines = allow_quoted_newlines  # type: ignore # noqa: E501
         external_config.csv_options.field_delimiter = field_delimiter  # type: ignore # noqa: E501
-        external_config.csv_options.allow_jagged_rows = allow_jagged_rows # type: ignore # noqa: E501
+        external_config.csv_options.allow_jagged_rows = allow_jagged_rows  # type: ignore # noqa: E501
 
     return external_config
 
